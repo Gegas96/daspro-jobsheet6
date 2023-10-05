cf720f36f77a03046a9c71d36394ae210d1160cd
Gegas96 <gegasanugrah@gmail.com>
2023-10-05 12:29:06 +0700

    jobsheet6

---
+++ Pemilihan2Percobaan211.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan211 {
+    public static void main(String[] args) {
+        
+        Scanner input11 = new Scanner(System.in);
+
+        float sudut1, sudut2, sudut3, totalSudut;
+
+        System.out.print("Masukkan sudut 1: ");
+        sudut1 = input11.nextFloat();
+        System.out.print("Masukkan sudut 2: ");
+        sudut2 = input11.nextFloat();
+        System.out.print("Masukkan sudut 3: ");
+        sudut3 = input11.nextFloat();
+
+        totalSudut = sudut1 + sudut2 + sudut3;
+
+        if (totalSudut == 180) {
+            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
+                System.out.println("Segita tersebut adalah segitiga siku-siku");
+            } else if ((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60)) {
+                System.out.println("Segita tersebut adalah segitiga sama sisi");
+            } else if ((sudut1==sudut2) || (sudut1==sudut3) || (sudut2==sudut3)) {
+                System.out.println("Segita tersebut adalah segitiga sama kaki");
+            } else {
+                System.out.println("Segita tersebut adalah segitiga sembarang");
+            }
+        } else {
+            System.out.println("Bukan segitiga");
+        }
+    }
+}
